cc76cd87359bd152f6c793abddc2d05ce73f992e
Evan Sun <epjsun@gmail.com>
2019-01-02 10:49:28 -0500

    i tried

--- js/pocket-calculator.js
+++ js/pocket-calculator.js
@@ -1,1 +1,85 @@
-var box;
+var box = "";
+var value = ""
+
+function num1(){
+  box = box + "1";
+  value = value + "1";
+  if(box.length > 12) box = box.substring(0, 12);
+  document.getElementById("output").innerHTML = box;
+  let p = "123456789";
+  let h = p.split("");
+  for(let i = p.length; i > 0; i=i-3){
+    
+  }
+  let j = h.join("");
+  console.log(j)
+}
+
+function num2(){
+  box = box + "2";
+  value = value + "2";
+  if(box.length > 12) box = box.substring(0, 12);
+  document.getElementById("output").innerHTML = box;
+}
+
+function num3(){
+  box = box + "3";
+  value = value + "3";
+  if(box.length > 12) box = box.substring(0, 12);
+  document.getElementById("output").innerHTML = box;
+}
+
+function num4(){
+  box = box + "4";
+  value = value + "4";
+  if(box.length > 12) box = box.substring(0, 12);
+  document.getElementById("output").innerHTML = box;
+}
+
+function num5(){
+  box = box + "5";
+  value = value + "5";
+  if(box.length > 12) box = box.substring(0, 12);
+  document.getElementById("output").innerHTML = box;
+}
+
+function num6(){
+  box = box + "6";
+  value = value + "6";
+  if(box.length > 12) box = box.substring(0, 12);
+  document.getElementById("output").innerHTML = box;
+}
+
+function num7(){
+  box = box + "7";
+  value = value + "7";
+  if(box.length > 12) box = box.substring(0, 12);
+  document.getElementById("output").innerHTML = box;
+}
+
+function num8(){
+  box = box + "8";
+  value = value + "8";
+  if(box.length > 12) box = box.substring(0, 12);
+  document.getElementById("output").innerHTML = box;
+}
+
+function num9(){
+  box = box + "9";
+  value = value + "9";
+  if(box.length > 12) box = box.substring(0, 12);
+  document.getElementById("output").innerHTML = box;
+}
+
+function num0(){
+  box = box + "0";
+  value = value + "0";
+  if(box.length > 12) box = box.substring(0, 12);
+  document.getElementById("output").innerHTML = box;
+}
+
+function ac(){
+  box = "";
+  value = "";
+  document.getElementById("output").innerHTML = box;
+}
