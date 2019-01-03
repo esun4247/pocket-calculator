5ae54e57f5b0ec264563a6cce99cde6124ab9c69
esun4247 <43784842+esun4247@users.noreply.github.com>
2019-01-03 09:12:41 -0500

    Update pocket-calculator.js

--- js/pocket-calculator.js
+++ js/pocket-calculator.js
@@ -4,7 +4,7 @@ var value = ""
 function num1(){
   box = box + "1";
   value = value + "1";
-  if(box.length > 12) box = box.substring(0, 12);
+  if(box.length > 11) box = box.substring(0, 11);
   document.getElementById("output").innerHTML = box;
   let p = "123456789";
   let h = p.split("");
@@ -18,63 +18,63 @@ function num1(){
 function num2(){
   box = box + "2";
   value = value + "2";
-  if(box.length > 12) box = box.substring(0, 12);
+  if(box.length > 11) box = box.substring(0, 11);
   document.getElementById("output").innerHTML = box;
 }
 
 function num3(){
   box = box + "3";
   value = value + "3";
-  if(box.length > 12) box = box.substring(0, 12);
+  if(box.length > 11) box = box.substring(0, 11);
   document.getElementById("output").innerHTML = box;
 }
 
 function num4(){
   box = box + "4";
   value = value + "4";
-  if(box.length > 12) box = box.substring(0, 12);
+  if(box.length > 11) box = box.substring(0, 11);
   document.getElementById("output").innerHTML = box;
 }
 
 function num5(){
   box = box + "5";
   value = value + "5";
-  if(box.length > 12) box = box.substring(0, 12);
+  if(box.length > 11) box = box.substring(0, 11);
   document.getElementById("output").innerHTML = box;
 }
 
 function num6(){
   box = box + "6";
   value = value + "6";
-  if(box.length > 12) box = box.substring(0, 12);
+  if(box.length > 11) box = box.substring(0, 11);
   document.getElementById("output").innerHTML = box;
 }
 
 function num7(){
   box = box + "7";
   value = value + "7";
-  if(box.length > 12) box = box.substring(0, 12);
+  if(box.length > 11) box = box.substring(0, 11);
   document.getElementById("output").innerHTML = box;
 }
 
 function num8(){
   box = box + "8";
   value = value + "8";
-  if(box.length > 12) box = box.substring(0, 12);
+  if(box.length > 11) box = box.substring(0, 11);
   document.getElementById("output").innerHTML = box;
 }
 
 function num9(){
   box = box + "9";
   value = value + "9";
-  if(box.length > 12) box = box.substring(0, 12);
+  if(box.length > 11) box = box.substring(0, 11);
   document.getElementById("output").innerHTML = box;
 }
 
 function num0(){
   box = box + "0";
   value = value + "0";
-  if(box.length > 12) box = box.substring(0, 12);
+  if(box.length > 11) box = box.substring(0, 11);
   document.getElementById("output").innerHTML = box;
 }
 
