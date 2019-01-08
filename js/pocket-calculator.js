0dd404db8ddd095f2d50f03a9e3d6eefc204596a
Evan Sun <epjsun@gmail.com>
2019-01-08 10:48:37 -0500

    neg pos percent started

--- js/pocket-calculator.js
+++ js/pocket-calculator.js
@@ -1,14 +1,24 @@
-document.getElementById("output").innerHTML = "0";
 var box = "";
 var value = "";
+var ignoreMinus = 0;
 
 function comma(){
-  if(box.length > 11) box = box.substring(0, 11);
+  if(Number(value) >= 0){
+    ignoreMinus = 0;
+  }else if(Number(value) < 0){
+    ignoreMinus = 1;
+  }
+  if(ignoreMinus == 0){
+    if(box.length > 11) box = box.substring(0, 11);
+  }else{
+    if(box.length > 12) box = box.substring(0, 12);
+  }
   let h = box.split("");
   let h1 = [];
   let h2 = [];
   let u = 0;
   let dec = 0;
+  let neg = 0;
 
   for(let j = box.length; j > 0; j--){
     if(h[j] === ","){
@@ -19,6 +29,13 @@ function comma(){
   for(let j = box.length; j > 0; j--){
     if(h[j-1] === "."){
       dec = 1;
+    }
+  }
+  for(let j = box.length; j >= 0; j--){
+    if(h[j] === "-"){
+      h.splice(j, 1);
+      neg = 1;
+      box = h.join("");
     }
   }
   if(dec == 1){
@@ -39,6 +56,9 @@ function comma(){
       u++;
     }
     box = h.join("");
+    if(neg == 1){
+    box = "-" + box;
+    }
   }
 }
 
@@ -46,7 +66,6 @@ function num1(){
   box = box + "1";
   value = value + "1";
   comma();
-  if(box.length > 11) box = box.substring(0, 11);
   document.getElementById("output").innerHTML = box;
 }
 
@@ -54,7 +73,6 @@ function num2(){
   box = box + "2";
   value = value + "2";
   comma();
-  if(box.length > 11) box = box.substring(0, 11);
   document.getElementById("output").innerHTML = box;
 }
 
@@ -62,7 +80,6 @@ function num3(){
   box = box + "3";
   value = value + "3";
   comma();
-  if(box.length > 11) box = box.substring(0, 11);
   document.getElementById("output").innerHTML = box;
 }
 
@@ -70,7 +87,6 @@ function num4(){
   box = box + "4";
   value = value + "4";
   comma();
-  if(box.length > 11) box = box.substring(0, 11);
   document.getElementById("output").innerHTML = box;
 }
 
@@ -78,7 +94,6 @@ function num5(){
   box = box + "5";
   value = value + "5";
   comma();
-  if(box.length > 11) box = box.substring(0, 11);
   document.getElementById("output").innerHTML = box;
 }
 
@@ -86,7 +101,6 @@ function num6(){
   box = box + "6";
   value = value + "6";
   comma();
-  if(box.length > 11) box = box.substring(0, 11);
   document.getElementById("output").innerHTML = box;
 }
 
@@ -94,7 +108,6 @@ function num7(){
   box = box + "7";
   value = value + "7";
   comma();
-  if(box.length > 11) box = box.substring(0, 11);
   document.getElementById("output").innerHTML = box;
 }
 
@@ -102,7 +115,6 @@ function num8(){
   box = box + "8";
   value = value + "8";
   comma();
-  if(box.length > 11) box = box.substring(0, 11);
   document.getElementById("output").innerHTML = box;
 }
 
@@ -110,7 +122,6 @@ function num9(){
   box = box + "9";
   value = value + "9";
   comma();
-  if(box.length > 11) box = box.substring(0, 11);
   document.getElementById("output").innerHTML = box;
 }
 
@@ -122,7 +133,6 @@ function num0(){
   value = value + "0";
   }
   comma();
-  if(box.length > 11) box = box.substring(0, 11);
   document.getElementById("output").innerHTML = box;
 }
 
@@ -130,12 +140,47 @@ function dec(){
   box = box + ".";
   value = value + ".";
   comma();
-  if(box.length > 11) box = box.substring(0, 11);
+  document.getElementById("output").innerHTML = box;
+}
+
+function plusMinus(){
+  if(Number(value) >= 0){
+    box = "-" + box;
+    value = "-" + value;
+    document.getElementById("output").innerHTML = box;
+  }else if(Number(value) < 0){
+    let o = box.split("");
+    for(let j = box.length; j >= 0; j--){
+      if(o[j] === "-"){
+        o.splice(j, 1);
+      }
+    }
+    box = o.join("");
+    let p = value.split("");
+    for(let j = box.length; j >= 0; j--){
+      if(p[j] === "-"){
+        p.splice(j, 1);
+      }
+    }
+    value = p.join("");
+    document.getElementById("output").innerHTML = box;
+  }
+}
+
+function percent(){
+  console.log(value);
+  value = Number(value) * .01;
+  value = value.toPrecision(5);
+  value = String(value);
+  console.log(value);
+  box = value;
+  comma();
   document.getElementById("output").innerHTML = box;
 }
 
 function ac(){
   box = "";
   value = "";
-  document.getElementById("output").innerHTML = box;
+  ignoreMinus = 0;
+  document.getElementById("output").innerHTML = "0";
 }
